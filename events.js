9aa333921fa293047968d32a6a181fa8822f6fe7
LIKHITH1827 <laddela@albany.edu>
2024-05-17 02:20:21 -0400

    modifying event,js

--- events.js
+++ events.js
@@ -13,3 +13,21 @@ const eventobj=new eventEmitter();
   })
 
  eventobj.emit('response');
+
+
+
+ const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  res.end('Welcome')
+})
+
+server.listen(5000)
