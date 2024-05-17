557b11dc949c5ad853f9c002e91cedbf9de77d36
LIKHITH1827 <laddela@albany.edu>
2024-05-17 01:08:50 -0400

    added some files

---
+++ inbuiltmodules.js
@@ -0,0 +1,13 @@
+//const os=require('os');
+//console.log(os.userInfo());
+
+//console.log("app files");
+
+//const show=require('./fundef_mod01');
+//const b=require('./dumy2');
+//const obj=require('./obje_mod01');
+
+//show(3);
+
+//console.log(obj.a);
+//console.log(obj.b);
